51d353e72a880f347d11aa35a22db6d592ed8390
thegodhope <103210115+thegodhope@users.noreply.github.com>
2022-10-31 02:50:17 +0100

    made a few changes

---
+++ src/components/data.js
@@ -0,0 +1,35 @@
+const slackUsername = "/python-for-beginners?ref_id=<Hopecodes>";
+
+export const links = [
+  {
+    name: "Twitter Link",
+    link: "https://twitter.com/Hope_thegod",
+    additionalText: "",
+  },
+  {
+    id: "btn_zuri",
+    name: "Zuri Team",
+    link: "https://training.zuri.team/",
+    additionalText: "",
+  },
+  {
+    id: "books",
+    name: "Zuri Books",
+    link: " https://books.zuri.team",
+  },
+  {
+    id: "book_python",
+    name: "Python Books",
+    link: `https://books.zuri.team${slackUsername}`,
+  },
+  {
+    id: "pitch",
+    name: "Background Check for Coders",
+    link: " https://background.zuri.team",
+  },
+  {
+    id: "book_design",
+    name: "Design Books",
+    link: "https://books.zuri.team/design-rules ",
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,69 @@
+import React from "react";
+import giticon from "../images/giticon.png";
+import slackicon from "../images/Slackicon.png";
+import zurilogo from "../images/zurilogo.png";
+import profileImage from "../images/mypicture.jpg";
+import zurisecondlogo from "../images/secondloogo.png";
+import { links } from "../components/data";
+import "../index.css";
+
+export default function () {
+  return (
+    <div>
+      {/* PROFILE SECTION */}
+
+      <div className="profilesection">
+        <div>
+          <img
+            id="profile_img"
+            src={profileImage}
+            alt="profile-pic"
+            className="w-88 h-90 rounded-full"
+          />
+        </div>
+
+        <div>
+          <h1 id="twitter"> Hope Etim </h1>
+        </div>
+      </div>
+
+      {/* PROFILE SECTION ENDS */}
+
+      <div className="linkssection">
+        {links.map((link, index) => {
+          return (
+            <div key={index} className="atag">
+              <a id={link.id} href={link.link} target={"_blank"}>
+                {link.name}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* SOCIAL MEDIA AND FOOTER */}
+      <div className="socials">
+        <a href="https://slack.com/Hopecodes" target={"_blank"}>
+          <img id="slack" src={slackicon} alt="git-icon" />
+        </a>
+
+        <a href="https://github.com/thegodhope" target="_blank">
+          <img src={giticon} alt="slack-icon" />
+        </a>
+      </div>
+
+      <footer className="footer">
+        <div className="footersection"></div>
+        <div>
+          <a href="https://zuri.com" target={"_blank"}>
+            <img src={zurilogo} alt="zuri-logo" />
+          </a>
+          <p className="footertext">HNG Internship 9 Frontend Task</p>
+          <a href="https://zuri.com" target={"_blank"}>
+            <img src={zurisecondlogo} alt="zuri-logo" className="" />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
